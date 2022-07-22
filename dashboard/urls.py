3bf309d2fdc70f1d5d97775000ea965cc9248691
Your Name <you@example.com>
2022-07-22 23:57:25 +0600

    add models note

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 
 from .import views
 
+app_name='dashboard_app'
 
 urlpatterns = [
     path('', views.HomePage, name='home'),
+    path('notes/', views.Notes, name='notes'),
 ]
     
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render,HttpResponse,HttpResponseRedirect
 def HomePage(request):
     return render(request,'dashboard/home.html')
 
+
+def Notes(request):
+    return render(request,'dashboard/notes.html')
