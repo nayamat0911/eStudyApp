dc0136afdaec0cea49f30d7d3f6acbb765553cc4
Your Name <you@example.com>
2022-07-30 01:01:11 +0600

    add profile

--- eStudyApp/urls.py
+++ eStudyApp/urls.py
@@ -5,12 +5,20 @@ from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns, static
 
 from dashboard import views as dash_views
+from django.contrib.auth import views as auth_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
-    path('register/', dash_views.register, name='register' )
+
+    path('register/', dash_views.register, name='register' ),
+    path('login/', auth_view.LoginView.as_view(template_name="dashboard/login.html"), name='login' ),
+
+    path('profile/', dash_views.profile, name='profile' ),
+
 ]
+
 urlpatterns += staticfiles_urlpatterns()
 if settings.DEBUG:
 
